7257e9a88add16068ea61ce321737f269d970b44
Oleg Bezkorovaynyi <Oleg.Bezkorovaynyi@Sophos.com>
2017-11-01 18:23:58 +0200

    add category service

--- src/main/java/guru/springfamework/repositories/CategoryRepository.java
+++ src/main/java/guru/springfamework/repositories/CategoryRepository.java
@@ -2,6 +2,10 @@ package guru.springfamework.repositories;
 
 import guru.springfamework.domain.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Category findByName(String name);
 }
---
+++ src/main/java/guru/springfamework/services/CategoryServiceImpl.java
@@ -0,0 +1,36 @@
+package guru.springfamework.services;
+
+import guru.springfamework.api.v1.mapper.CategoryMapper;
+import guru.springfamework.api.v1.model.CategoryDTO;
+import guru.springfamework.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    private final CategoryMapper mapper;
+
+    @Autowired
+    public CategoryServiceImpl(final CategoryRepository categoryRepository, final CategoryMapper mapper) {
+        this.categoryRepository = categoryRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public List<CategoryDTO> getAllCategories() {
+        return categoryRepository.findAll().stream()
+                .map(mapper::categoryToCategoryDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CategoryDTO getCategoryByName(final String name) {
+        return mapper.categoryToCategoryDTO(categoryRepository.findByName(name));
+    }
+}
